4ea706cd40737ece57c987e78e9732258eab974c
EPICGameGuy <admin@cactus.vg>
2023-04-16 02:11:07 -0400

    Embedded boost Removed SDL

--- Source/ThirdParty/EngineSim/EngineSim.Build.cs
+++ Source/ThirdParty/EngineSim/EngineSim.Build.cs
@@ -23,7 +23,7 @@ public class EngineSim : ModuleRules
 
         throw new BuildException("Unsupported platform");
     }
-    
+
     private string GetBoostVersion(ReadOnlyTargetRules Target)
     {
         if (Target.Platform == UnrealTargetPlatform.Win64)
@@ -58,123 +58,31 @@ public class EngineSim : ModuleRules
 
     private string GetBoostIncludePath(ReadOnlyTargetRules Target)
     {
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            // If on windows, manually specify the path here
-            return "C:/local/boost_1_70_0";
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Linux)
-        {
-            return "/usr/include";
-        }
-
-        throw new BuildException("Unsupported platform");
+        return Path.Combine(GetEngineSimPath(Target), "include/");
     }
 
     private string GetBoostLibPath(ReadOnlyTargetRules Target)
     {
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            // If on windows, manually specify the path here
-            return "C:/local/boost_1_70_0/lib64-msvc-14.2";
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Linux)
-        {
-            return "/usr/lib/x86_64-linux-gnu";
-
-            // This might also be in:
-            // /usr/lib
-            // /usr/lib64
-        }
-
-        throw new BuildException("Unsupported platform");
-    }
-
-    private string GetSDL2IncludePath(ReadOnlyTargetRules Target)
-    {
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            // If on windows, manually specify the path here
-            return "C:/local/SDL2/include";
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Linux)
-        {
-            return "/usr/include/SD2";
-        }
-
-        throw new BuildException("Unsupported platform");
-    }
-
-    private string GetSDL2LibPath(ReadOnlyTargetRules Target)
-    {
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            // If on windows, manually specify the path here
-            return "C:/local/SDL2/lib/x64";
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Linux)
-        {
-            return "/usr/lib/x86_64-linux-gnu";
-
-            // This might also be in:
-            // /usr/lib
-            // /usr/lib64
-        }
-
-        throw new BuildException("Unsupported platform");
-    }
-
-    private string GetSDL2ImageIncludePath(ReadOnlyTargetRules Target)
-    {
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            // If on windows, manually specify the path here
-            return "C:/local/SDL2_image/include";
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Linux)
-        {
-            return GetSDL2IncludePath(Target);
-        }
-
-        throw new BuildException("Unsupported platform");
-    }
-
-    private string GetSDL2ImageLibPath(ReadOnlyTargetRules Target)
-    {
-        if (Target.Platform == UnrealTargetPlatform.Win64)
-        {
-            // If on windows, manually specify the path here
-            return "C:/local/SDL2_image/lib/x64";
-        }
-        else if (Target.Platform == UnrealTargetPlatform.Linux)
-        {
-            return "/usr/lib/x86_64-linux-gnu";
-
-            // This might also be in:
-            // /usr/lib
-            // /usr/lib64
-        }
-
-        throw new BuildException("Unsupported platform");
+        return Path.Combine(GetEngineSimPath(Target), "libs/");
     }
 
     private string ModulePath
-	{
-		get { return ModuleDirectory; }
-	}
+    {
+        get { return ModuleDirectory; }
+    }
 
-	public EngineSim(ReadOnlyTargetRules Target) : base(Target)
-	{
+    public EngineSim(ReadOnlyTargetRules Target) : base(Target)
+    {
         Type = ModuleType.External;
 
         var libPath = Path.Combine(GetEngineSimPath(Target), "libs/");
 
-        String EngineSimLibPath                  = Path.Combine(libPath, "engine-sim.lib");
+        String EngineSimLibPath = Path.Combine(libPath, "engine-sim.lib");
         String EngineSimScriptInterpreterLibPath = Path.Combine(libPath, "engine-sim-script-interpreter.lib");
-        String EngineSimAppLibPath               = Path.Combine(libPath, "engine-sim-app-lib.lib");
-        String ConstraintResolverLibPath         = Path.Combine(libPath, "simple-2d-constraint-solver.lib");
-        String PiranhaLibPath                    = Path.Combine(libPath, "piranha.lib");
-        String DeltaStudioLibPath                = Path.Combine(libPath, "delta-core.lib");
+        String EngineSimAppLibPath = Path.Combine(libPath, "engine-sim-app-lib.lib");
+        String ConstraintResolverLibPath = Path.Combine(libPath, "simple-2d-constraint-solver.lib");
+        String PiranhaLibPath = Path.Combine(libPath, "piranha.lib");
+        String DeltaStudioLibPath = Path.Combine(libPath, "delta-core.lib");
 
         PublicDefinitions.Add("ATG_ENGINE_SIM_PIRANHA_ENABLED=1");
 
@@ -193,8 +101,8 @@ public class EngineSim : ModuleRules
         PublicAdditionalLibraries.Add(ConstraintResolverLibPath);
         PublicAdditionalLibraries.Add(PiranhaLibPath);
         PublicAdditionalLibraries.Add(DeltaStudioLibPath);
+
         LinkBoost(Target);
-        LinkSDL2(Target);
     }
 
     private void LinkBoost(ReadOnlyTargetRules target)
@@ -203,8 +111,6 @@ public class EngineSim : ModuleRules
 
         string BoostVersion = GetBoostVersion(target);
         string BoostVersionDir = "boost-" + BoostVersion;
-
-        string BoostIncludePath = GetBoostIncludePath(target);
 
         if (target.Platform == UnrealTargetPlatform.Win64)
         {
@@ -227,34 +133,6 @@ public class EngineSim : ModuleRules
                 string BoostLibName = "libboost_" + BoostLib;
                 PublicAdditionalLibraries.Add(Path.Combine(BoostLibPath, BoostLibName + ".a"));
             }
-        }
-    }
-
-    private void LinkSDL2(ReadOnlyTargetRules target)
-    {
-        if (target.Platform == UnrealTargetPlatform.Win64)
-        {
-            string SDL2LibPath = Path.Combine(GetSDL2LibPath(target), "SDL2.lib");
-            string SDL2DLLPath = Path.Combine(GetSDL2LibPath(target), "SDL2main.dll");
-            string SDL2MainLibPath = Path.Combine(GetSDL2LibPath(target), "SDL2main.lib");
-            PublicAdditionalLibraries.Add(SDL2LibPath);
-            PublicAdditionalLibraries.Add(SDL2MainLibPath);
-
-            string SDL2ImageLibPath = Path.Combine(GetSDL2ImageLibPath(target), "SDL2_image.lib");
-            string SDL2ImageDLLPath = Path.Combine(GetSDL2ImageLibPath(target), "SDL2_image.dll");
-            PublicAdditionalLibraries.Add(SDL2ImageLibPath);
-        }
-        else if (target.Platform == UnrealTargetPlatform.Linux)
-        {
-            string SDL2LibPath = Path.Combine(GetSDL2LibPath(target), "libSDL2.a");
-            string SDL2DLLPath = Path.Combine(GetSDL2LibPath(target), "libSDL2main.so");
-            string SDL2MainLibPath = Path.Combine(GetSDL2LibPath(target), "libSDL2main.a");
-            PublicAdditionalLibraries.Add(SDL2LibPath);
-            PublicAdditionalLibraries.Add(SDL2MainLibPath);
-
-            string SDL2ImageLibPath = Path.Combine(GetSDL2ImageLibPath(target), "libSDL2_image.a");
-            string SDL2ImageDLLPath = Path.Combine(GetSDL2ImageLibPath(target), "libSDL2_image.so");
-            PublicAdditionalLibraries.Add(SDL2ImageLibPath);
         }
     }
 }
